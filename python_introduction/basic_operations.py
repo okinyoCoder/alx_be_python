f06a9d3265c6b736d38e5549f2ca0b5f2319fe1c
okinyoCoder <okinyoanthony@gmail.com>
2024-12-15 22:57:24 +0300

    made changes

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -5,6 +5,6 @@ add = number1 + number2
 diff = number2 - number1
 multi = number1 * number2
 
-print("Addition of " number1 " and " number2 " is" add)
-print("Subtraction of " number2 " and " number1 " is" diff)
-print("Multiplication of " number1 " and " number2 " is" multi)
+print(f"Addition of {number1} and {number2} is {add}")
+print(f"Subtraction of {number2} and {number1} is {diff}")
+print(f"Multiplication of {number1} and {number2} is {multi}")
