d73313fb98ff614d212a22df76ab59ff9548d410
okinyoCoder <okinyoanthony@gmail.com>
2024-12-15 22:31:38 +0300

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+add = number1 + number2
+diff = number2 - number1
+multi = number1 * number2
+
+print("Addition of " number1 " and " number2 " is" add)
+print("Subtraction of " number2 " and " number1 " is" diff)
+print("Multiplication of " number1 " and " number2 " is" multi)
